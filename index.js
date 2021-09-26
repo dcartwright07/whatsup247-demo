3975f29d7a2194d63db8f1d51076bfd9ed4f055e
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-09-25 20:18:42 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const fetchEvents = async () => {
+  console.log('Get Events!')
+}
+
+const fetchEvent = async () => {
+  console.log('Get Event!')
+}
+
+const fetchDirectory = async () => {
+  console.log('Get Directory!')
+}
+
+const fetchOrganization = async () => {
+  console.log('Get Organization!')
+}
+
+const fetchDestinations = async () => {
+  console.log('Get Destinations!')
+}
+
+const fetchDestination = async () => {
+  console.log('Get Destination!')
+}
+
+module.exports = {
+  fetchEvents,
+  fetchEvent,
+  fetchDirectory,
+  fetchOrganization,
+  fetchDestinations,
+  fetchDestination,
+}
